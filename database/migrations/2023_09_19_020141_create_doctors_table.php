513bad7853e8b559bbe9ecfe6264f10c452a1eb1
juanwallss <juancparedesg@gmail.com>
2023-09-18 21:06:04 -0700

    added doctor migration

--- database/migrations/2023_09_19_020141_create_doctors_table.php
+++ database/migrations/2023_09_19_020141_create_doctors_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('doctors', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('specialty_id')->references('id')->on('specialties');
+            $table->string('name');
+            $table->string('father_lastname');
+            $table->string('mother_lastname')->nullable();
+            $table->string('professional_id');
+            $table->string('phone');
+            $table->tinyInteger('hired');
             $table->timestamps();
         });
     }
