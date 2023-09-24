a001b4f322c5943d6eefcd820085aa83ef2c0691
KarimSoto <karimtec2003@gmail.com>
2023-09-23 18:19:50 -0700

    cree la tabla drugs, treatments, sales, treatmentdrugs

---
+++ database/migrations/2023_09_23_235532_create_sales_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->id();
+            $table->date('sale_date');
+            $table->enum('status', ['COMPLETA', 'INCOMPLETA',])->default('INCOMPLETA');
+            $table->foreignId('appointment_id')->unique()->references('id')->on('appointments');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sales');
+    }
+};
